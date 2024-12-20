75ce6f1a631134b8c57a676a1f64709477cb2618
Hassanssain <mh268171@gmail.com>
2024-12-21 00:13:04 +0500

    new

---
+++ new.js
@@ -0,0 +1,24 @@
+let todos = []
+
+let alpha =prompt("what do you wanna do today")
+
+todos.push(alpha)
+
+let beta =prompt("what do you wanna do today")
+
+todos.push(beta)
+
+let gama =prompt("what do you wanna do today")
+
+todos.push(gama)
+
+let rays =prompt("what do you wanna do today")
+
+todos.push(rays)
+
+console.log ("your-todo-list");
+
+console.log(todos);
+
+
+
